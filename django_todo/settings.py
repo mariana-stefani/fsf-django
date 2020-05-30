a68357a9fd2f146fbc3c06459371a14e291d9cee
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-30 17:35:27 +0100

    Updated allowed hosts on settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','ftvs8apx4ngfpub#2ur!pxykv$5%@tr!sae$wf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['macs-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
