7d934f9906fc07af2fc985b8294f608709a313c3
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-30 17:05:55 +0100

    Added postgres database though dj_database_url to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://jfadtxndpksbol:0800340b04ea7337c5bbe25cae7d82764c5eb7fc08753ecbf8859a558f02b570@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d8k2kbt0onjbp1')
 }
 
 
