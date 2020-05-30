eca83537e01e73e1c6c47448daf837924ebd0a9d
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-30 14:46:19 +0100

    Added test_fields_are_explicit_in_form_metaclass method to test_forms.py

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -13,3 +13,7 @@ class TestItemForm(TestCase):
     def test_done_field_is_not_required(self):
         form = ItemForm({'name': 'Test Todo Item'})
         self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ItemForm()
+        self.assertEqual(form.Meta.fields, ['name', 'done'])
