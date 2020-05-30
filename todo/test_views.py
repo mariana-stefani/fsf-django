318954fde93c0215dc87b3d4ec3c3e3c8cf249cd
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-30 15:29:24 +0100

    Added test_get_edit_item_page method to TestView class function on todo/test_views.py

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Item
 
 
 class TestViews(TestCase):
@@ -12,3 +13,9 @@ class TestViews(TestCase):
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
