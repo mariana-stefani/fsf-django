6f6e24f505779c3d9a37a33f8c6615eb94925d5f
Mariana Stefani <mariana.camargo@hotmail.co.uk>
2020-05-30 15:48:21 +0100

    Added test_can_toggle_item method to TestViews class funtion on todo/test_views.py

--- todo/test_views.py
+++ todo/test_views.py
@@ -30,3 +30,10 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         existing_items = Item.objects.filter(id=item.id)
         self.assertEqual(len(existing_items), 0)
+
+    def test_can_toggle_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=True)
+        response = self.client.get(f'/toggle/{item.id}')
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertFalse(updated_item.done)
